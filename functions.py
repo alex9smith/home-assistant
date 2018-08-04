868302851596f3d800443494b2230c72039a5367
Alex Smith <alex.me.smith@gmail.com>
2018-08-04 13:31:36 +0100

    Added core API routes

--- functions.py
+++ functions.py
@@ -1,3 +1,222 @@
 # Contains the common functions and classes
 
 from variables import *
+import sys
+from pymongo import MongoClient
+from datetime import datetime
+
+## Helper functions
+
+def is_valid_storage_id(id):
+    # Checks to see if the supplied id is a valid storage id
+    # Returns true or false
+    if type(id) == str and '|' in id:
+        return True
+
+    else:
+        return False
+
+## Database interactions
+
+# Shopping List
+
+def get_all_shopping_list_items():
+    # Retrieves all items on the shopping list from Mongo
+    # Returns a list of dictionaries
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['shoppinglist']
+    items = collection.find()
+    if items:
+        items = [i for i in items]
+        client.close()
+        return items
+    else:
+        client.close()
+        return []
+
+def add_item_to_shopping_list(id):
+    # Takes an item name and adds it to the shopping list collection in Mongo
+    # Returns the inserted item
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['shoppinglist']
+    item = {
+        '_id': str(id).lower().replace(' ', '-'),
+        'name': str(id),
+        'added': datetime.now().strftime("%s")
+    }
+    collection.insert_one(item)
+    client.close()
+    return [item]
+
+def delete_item_from_shopping_list(id):
+    # Takes an item ID and deletes it from the shopping list
+    # Returns the deleted item or None if not found
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['shoppinglist']
+    query = {
+        '_id': id
+    }
+    found = collection.find_one(query)
+    if found:
+        collection.delete_one(query)
+        client.close()
+        return [found]
+    else:
+        client.close()
+        return None
+
+def search_shopping_list_for_item(id):
+    # Takes an ID and searches the shopping list for that item
+    # Returns the first matched item, or None if there are no matches
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['shoppinglist']
+    query = {
+        '_id': id
+    }
+    found = collection.find_one(query)
+    if found:
+        found = [i for i in found]
+        client.close()
+        return found
+    else:
+        client.close()
+        return None
+
+## Storage
+
+def get_all_stored_items():
+    # Connects to the storage Mongo collection and returns a list of all stored items
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    items = collection.find()
+    if items:
+        items = [i for i in items]
+        client.close()
+        return items
+    else:
+        client.close()
+        return []
+
+def add_item_to_storage(name, location):
+    # Takes an item name and location
+    # Adds the item to Mongo
+    # Returns the added item
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    item = {
+        '_id': str(name).lower().replace(' ', '-') + '|' + str(location).lower().replace(' ', '-'),
+        'name': str(name).lower(),
+        'location': str(location),
+        'added': datetime.now().strftime("%s")
+    }
+    collection.insert_one(item)
+    client.close()
+    return [item]
+
+def search_stored_items_by_name(name):
+    # Takes an item name and searches the collection with it
+    # returns a list of matched items or None 
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    query = {
+        'name': name
+    }
+    found = collection.find(query)
+    if found:
+        found = [i for i in found]
+        client.close()
+        return found
+    else:
+        client.close()
+        return None
+
+def search_stored_items_by_id(id):
+    # Takes an item ID of the form name|location and searches for it
+    # Returns a list containing the matched item or None if no matches
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    query = {
+        '_id': str(id)
+    }
+    found = collection.find(query)
+    found = [i for i in found]
+    if found:
+        found = [i for i in found]
+        client.close()
+        return found
+    else:
+        client.close()
+        return None
+
+def delete_from_storage_by_name(name):
+    # Takes an item name and searches the collection for it
+    # if a single match is found, deletes the item and returns a list containing the matched items
+    # if multiple matches are found, does not delete and returns a list of matched items
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    query = {
+        'name': {'$regex': '.*' + str(name) + '.*'}
+    }
+    found = collection.find(query)
+    if found == None:
+        return None
+    else:
+        items = [i for i in found]
+        if len(items) == 1:
+            collection.delete_one(query)
+        return items
+
+def delete_from_storage_by_id(id):
+    # Takes an item ID and searches the collection for it
+    # if a match is found, deletes the item and returns a list containing the matched item
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    query = {
+        '_id': str(id)
+    }
+    found = collection.find_one(query)
+    if found == None:
+        client.close()
+        return None
+    else:
+        collection.delete_one(query)
+        client.close()
+        return [items]
+
+def get_all_storage_locations():
+    # Queries Mongo for distinct locations in use
+    # Returns these as a list
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    locations = collection.find().distinct('location')
+    client.close()
+    return locations
+
+def get_items_stored_in_location(location):
+    # Queries Mongo for all items with a particular location value
+    # Returns a list of items, or an empty list if no matches
+    client = MongoClient(host = MONGO_DB_HOST, port = MONGO_DB_PORT, username = MONGO_DB_USER, password = MONGO_DB_PASS, authSource = MONGO_DB_DBASE)
+    db = client[MONGO_DB_DBASE]
+    collection = db['storage']
+    query = {
+        'location': location
+    }
+    items = collection.find(query)
+    if items:
+        items = [i for i in collection.find(query)]
+        client.close()
+        return items
+    else:
+        client.close()
+        return []
--- main.py
+++ main.py
@@ -1,10 +1,222 @@
 # Contains the main structure of the Flask app
-from flask import Flask
+from flask import Flask, Response, request
 from functions import *
 from variables import *
+
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World"
+
+## API Routes
+
+## Shopping List
+
+@app.route('/api/v1/shoppinglist')
+def api_show_shopping_list():
+    status_code = 200
+    response = {}
+
+    try:
+        response = {
+            'status': 'Success',
+            'items': get_all_shopping_list_items()
+        }
+
+    except Exception as e:
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+
+@app.route('/api/v1/shoppinglist/<id>', methods = ['GET', 'PUT', 'DELETE'])
+def api_manage_shopping_list(id):
+
+    status_code = 200
+    response = {}
+
+    try:
+
+        if request.method == 'GET':
+            # search the list for an item
+            item = search_shopping_list_for_item(id)
+            if item:
+                response = {
+                    'status': 'Success',
+                    'item': item
+                }
+
+            else:
+                raise FileNotFoundError('No item found with that ID')
+
+        elif request.method == 'PUT':
+            # add a new item to the list
+            item = add_item_to_shopping_list(id)
+            response = {
+                'status': 'Success',
+                'item': item
+            }
+
+        elif request.method == 'DELETE':
+            # delete an item from the list
+
+            deleted = delete_item_from_shopping_list(id)
+            if deleted:
+                response = {
+                    'status': 'Success',
+                    'id': id
+                }
+
+            else:
+                raise FileNotFoundError('No item found with that ID')
+        
+    except Exception as e:
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+
+## Storage 
+
+@app.route('/api/v1/storage')
+def api_show_stored_items():
+
+    status_code = 200
+    response = {}
+
+    try:
+        response = {
+            'status': 'Success',
+            'items': get_all_stored_items()
+        }
+
+    except Exception as e:
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+
+@app.route('/api/v1/storage/<id>', methods = ['GET', 'PUT', 'DELETE'])
+def api_manage_storage(id):
+    # ID here can either be the name or the Mongo ID of name|location
+    # Need to handle this in each method where it's relevant
+
+    status_code = 200
+    response = {}
+
+    try:
+
+        if request.method == 'GET':
+            # Search for the item
+            if is_valid_storage_id(id):
+                response = {
+                    'status': 'Success',
+                    'item': search_stored_items_by_id(id)
+                }
+
+            else:
+                response = {
+                    'status': 'Success',
+                    'item': search_stored_items_by_name(id)
+                }
+
+        elif request.method == 'PUT':
+            # Add the item
+            if is_valid_storage_id(id):
+
+                name, location = id.split('|')
+                response = {
+                    'status': 'Success',
+                    'item': add_item_to_storage(name, location)
+                }
+            else:
+                raise NameError('The provided ID is not valid. Name and location are both reqiured.')
+
+        elif request.method == 'DELETE':
+            # Delete the item
+            if is_valid_storage_id(id):
+
+                deleted = delete_from_storage_by_id(id)       
+                if deleted:
+                    response = {
+                        'status': 'Success',
+                        'item': deleted
+                    }
+                else:
+                    raise FileNotFoundError('No item found with that ID') 
+
+            else:
+                deleted = delete_from_storage_by_name(id)       
+                if deleted:
+                    if len(deleted) == 1:
+                        response = {
+                            'status': 'Success',
+                            'item': deleted
+                        }
+                    else:
+                        raise FileExistsError('Multiple items with that name were found. Please specify a location as well')
+                else:
+                    raise FileNotFoundError('No item found with that ID')    
+
+    except Exception as e:
+
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+
+@app.route('/api/v1/storage/locations')
+def api_list_locations():
+    status_code = 200
+    response = {}
+
+    try:
+        response = {
+            'status': 'Success',
+            'locations': get_all_storage_locations()
+        }
+
+    except Exception as e:
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+
+@app.route('/api/v1/storage/locations/<location>')
+def api_list_items_in_location(location):
+    status_code = 200
+    response = {}
+
+    try:
+        response = {
+            'status': 'Success',
+            'locations': get_items_stored_in_location(location)
+        }
+
+    except Exception as e:
+        status_code = 500
+        response = {
+            'status': 'Error',
+            'details': str(e)
+        }
+
+    return Response(json.dumps(response), mimetype='application/json', status = status_code)
+        
