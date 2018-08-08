d31533e8d1d982d2c6fc783567335ebc8023c03d
Alex Smith <alex.me.smith@gmail.com>
2018-08-08 22:07:10 +0100

    Create variables.py

---
+++ app/variables.py
@@ -0,0 +1,6 @@
+# Contains all the environment variables required
+# At some point these will be moved into the Dockerfile
+MONGO_DB_HOST = ''
+MONGO_DB_PORT = ''
+MONGO_DB_USER = ''
+MONGO_DB_PASS = ''
